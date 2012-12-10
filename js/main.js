56cf56f7be30de8820583c3850e8da25a98741de
Malcolm Inglis <me@minglis.id.au>
2012-12-10 18:46:24 -0500

    Add basic Backbone code, move Bootstrap CSS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
+$(function(){
+  var Router = Backbone.Router.extend({
+    routes: {
+      '': 'file',
+      'file': 'file',
+      'overview': 'overview'
+    },
 
+    file: function() {
+      return (new FileView()).render();
+    },
+
+    overview: function() {
+      return (new OverviewView()).render();
+    }
+  });
+
+  var FileView = Backbone.View.extend({
+    el: '#container',
+    template: _.template($('#file-template').html()),
+    render: function() {
+      this.$el.html(this.template());
+      return this;
+    },
+  });
+
+  var OverviewView = Backbone.View.extend({
+    el: '#container',
+    template: _.template($('#overview-template').html()),
+    render: function() {
+      this.$el.html(this.template());
+      return this;
+    }
+  });
+
+  new Router();
+  Backbone.history.start();
+})
