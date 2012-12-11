f23a1f151c428881cc4bb8cf8ab9ea8a6adce653
Krys <aknadresneg@gmail.com>
2012-12-11 06:03:01 -0500

    Add cross.js a fabric object class for a cross

---
+++ js/cross.js
@@ -0,0 +1,19 @@
+var Cross = fabric.util.createClass(fabric.Object, {
+
+  initialize: function(options) {
+    this.callSuper('initialize', options);
+
+    this.width = 10;
+    this.height = 10;
+
+    this.w1 = this.h2 = 20;
+    this.h1 = this.w2 = 2;
+  },
+
+  _render: function(ctx) {
+    ctx.fillRect(-this.w1 / 2, -this.h1 / 2, this.w1, this.h1);
+    ctx.fillRect(-this.w2 / 2, -this.h2 / 2, this.w2, this.h2);
+	ctx.fill();
+  }
+  
+});
