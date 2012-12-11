5a1549a9c7c6a6a4137c445e2534c2c84ebf86ff
Malcolm Inglis <me@minglis.id.au>
2012-12-11 01:29:28 -0500

    Implement basic navigational framework

--- js/main.js
+++ js/main.js
@@ -1,38 +1,105 @@
-$(function(){
+$(function() {
+
+  function template(identifier) {
+    return _.template($(identifier).html());
+  }
+
+  var Navigation = Backbone.Model.extend({
+    defaults: function() {
+      return {
+        previous: false,
+        current: 'start',
+        next: 'overview'
+      };
+    },
+
+    cycle: function(newNext) {
+      this.set({
+        previous: this.get('current'),
+        current: this.get('next'),
+        next: newNext,
+      });
+    }
+  });
+
+  var NavigationView = Backbone.View.extend({
+    el: '#navigation',
+    template: template('#navigation-template'),
+
+    events: {
+      'click #nav-next': 'nextScreen',
+      'click #nav-previous': 'previousScreen'
+    },
+
+    initialize: function() {
+      this.model.bind('change', this.render, this);
+    },
+
+    render: function() {
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
+    },
+
+    setNextScreen: function(screen) {
+      this.model.cycle(screen);
+    },
+
+    nextScreen: function() {
+
+    },
+
+    previousScreen: function() {},
+  });
+
+  var ScreenView = Backbone.View.extend({
+    el: '#dynamic',
+  });
+
+  var StartView = ScreenView.extend({
+    template: template('#start-template'),
+    render: function() {
+      this.$el.html(this.template());
+      return this;
+    },
+  });
+
+  var OverviewView = ScreenView.extend({
+    template: template('#overview-template'),
+    render: function() {
+      this.$el.html(this.template());
+      return this;
+    }
+  });
+
   var Router = Backbone.Router.extend({
+    views: {
+      navigation: new NavigationView({ model: new Navigation() }),
+      start: new StartView(),
+      overview: new OverviewView()
+    },
+
+    initialize: function(options) {
+      this.views.navigation.render();
+    },
+
     routes: {
-      '': 'file',
-      'file': 'file',
+      '': 'start',
+      'start': 'start',
       'overview': 'overview'
     },
 
-    file: function() {
-      return (new FileView()).render();
+    start: function() {
+      return this.views.start.render();
     },
 
     overview: function() {
-      return (new OverviewView()).render();
+      this.views.navigation.setNextScreen('blerh');
+      return this.views.overview.render();
     }
   });
 
-  var FileView = Backbone.View.extend({
-    el: '#container',
-    template: _.template($('#file-template').html()),
-    render: function() {
-      this.$el.html(this.template());
-      return this;
-    },
-  });
+  router = new Router();
+  router.on('all', function(name) { console.log(name); });
 
-  var OverviewView = Backbone.View.extend({
-    el: '#container',
-    template: _.template($('#overview-template').html()),
-    render: function() {
-      this.$el.html(this.template());
-      return this;
-    }
-  });
-
-  new Router();
   Backbone.history.start();
-})
+});
