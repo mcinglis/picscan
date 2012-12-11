5feaf28377b9b01394d92b575fd8ff462b994924
Malcolm Inglis <me@minglis.id.au>
2012-12-11 06:49:47 -0500

    Extend work on canvas sizing, and merge Krys' work

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 
-if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
-  alert('The file APIs are not supported by your browser.');
+window.URL = window.URL || window.webkitURL;
+
+required_apis = [
+  window.File, window.FileReader, window.FileList, window.Blob, window.URL 
+];
+
+if (!(_.every(required_apis, _.identity))) {
+  alert('The required APIs are not supported by your browser.');
 }
+
+var MAX_CANVAS_HEIGHT = 400, MAX_CANVAS_WIDTH = 1100;
 
 $(function() {
 
@@ -16,89 +24,116 @@ $(function() {
     },
   };
 
-  var ScreenView = Backbone.View.extend({ el: '#dynamic', });
+  var ScreenView = Backbone.View.extend({
+    el: '#dynamic',
+  });
 
   var StartView = ScreenView.extend({
     template: template('#start-template'),
 
     render: function() {
       this.$el.html(this.template());
-      this.saveSize();
+      this.saveRatio();
       return this;
     },
 
     events: {
-      'change #size': 'saveSize',
+      'change #file': 'saveImageURL',
+      'change #ratio': 'saveRatio',
     },
-      
-    saveSize: function() {
-        var value = parseFloat($('#size :selected').attr('value'));
-        configuration.set('sizeRatio', value);
+
+    saveImageURL: function() {
+      var element = $('#file')[0],
+          url = URL.createObjectURL(element.files[0]);
+      configuration.set('imageURL', url);
+    },
+
+    saveRatio: function() {
+      var element = $('#ratio :selected'),
+          value = parseFloat(element.attr('value'));
+      configuration.set('sizeRatio', value);
     },
   });
 
   var OverviewView = ScreenView.extend({
     template: template('#overview-template'),
+
     render: function() {
-		this.$el.html(this.template());
-		this.canvas = new fabric.StaticCanvas('overview');
-		
-		var p1 = configuration.
-		
-		var line1 = this.makeEdge([ tl.x, tl.y, tr.x, tr.y]),
-		var line2 = this.makeEdge([ tr.x, tr.y, br.x, br.y]),
-		var line3 = this.makeEdge([ br.x, br.y, bl.x, bl.y]),
-		var line4 = this.makeEdge([ bl.x, bl.y, tl.x, tl.y]);
-
-		this.canvas.add(line, line2, line3, line4);
-
-		this.canvas.add(
-			this.makeCorner(line1.get('x1'), line1.get('y1'), line1, line2),
-			this.makeCorner(line2.get('x2'), line2.get('y2'), line2, line3),
-			this.makeCorner(line3.get('x2'), line3.get('y2'), line3, line4),
-			this.makeCorner(line4.get('x2'), line4.get('y2'), line4, line1),
-		);
-		
-		this.canvas.on('object:moving', function(e) {
-			var p = e.target;
-			p.line1 && p.line1.set({ 'x2': p.left, 'y2': p.top });
-			p.line2 && p.line2.set({ 'x1': p.left, 'y1': p.top });
-			this.canvas.renderAll();
-		});
+      this.$el.html(this.template());
+      this.renderCanvas();
       return this;
     },
-	
+
+    renderCanvas: function() {
+      this.canvas = new fabric.Canvas('overview', { selection: false });
+      var canvas = this.canvas;
+
+      fabric.Image.fromURL(configuration.imageURL, function(image) {
+        var heightRatio = MAX_CANVAS_HEIGHT / image.height,
+            widthRatio = MAX_CANVAS_WIDTH / image.width;
+        configuration.set('overviewHeightRatio', heightRatio);
+        configuration.set('overviewWidthRatio', widthRatio);
+        if (heightRatio < widthRatio) {
+          image.scale(heightRatio);
+          canvas.setWidth(image.width * heightRatio);
+        } else {
+          image.scale(widthRatio);
+          canvas.setHeight(image.height * widthRatio);
+        }
+        canvas.add(image);
+        image.center();
+        image.selectable = false;
+        canvas.renderAll();
+      });
+
+      this.renderCanvasCursors();
+    },
+
+    renderCanvasCursors: function() {
+      var tl = configuration.overviewTL,
+          tr = configuration.overviewTR,
+          br = configuration.overviewBR,
+          bl = configuration.overviewBL,
+          line1 = this.makeEdge([ tl.x, tl.y, tr.x, tr.y]),
+          line2 = this.makeEdge([ tr.x, tr.y, br.x, br.y]),
+          line3 = this.makeEdge([ br.x, br.y, bl.x, bl.y]),
+          line4 = this.makeEdge([ bl.x, bl.y, tl.x, tl.y]);
+
+      this.canvas.add(line, line2, line3, line4);
+
+      this.canvas.add(
+        this.makeCorner(line1.get('x1'), line1.get('y1'), line1, line2),
+        this.makeCorner(line2.get('x2'), line2.get('y2'), line2, line3),
+        this.makeCorner(line3.get('x2'), line3.get('y2'), line3, line4),
+        this.makeCorner(line4.get('x2'), line4.get('y2'), line4, line1)
+      );
+
+      this.canvas.on('object:moving', function(e) {
+        var p = e.target;
+        p.line1 && p.line1.set({ 'x2': p.left, 'y2': p.top });
+        p.line2 && p.line2.set({ 'x1': p.left, 'y1': p.top });
+        this.canvas.renderAll();
+      });
+    },
 	
 	makeCorner: function (centerX, centerY, lineCounter, lineClock) {
-		var c = new Cross({
-			top: centerY,
-			left: centerX,
-			fill: 'red',
-		});
-		
-		c.hasControls = c.hasBorders = false;
-
-		c.line1 = line1;
-		c.line2 = line2;
-
-		return c;
+      var c = new Cross({ top: centerY, left: centerX, fill: 'red' });
+      c.hasControls = c.hasBorders = false;
+      c.line1 = lineCounter;
+      c.line2 = lineClock;
+      return c;
 	},
 
 	makeEdge: function (coords) {
-		return new fabric.Line(coords, {
-			fill: 'yellow',
-			strokeWidth: 1,
-			selectable: false
-		});
+      return new fabric.Line(coords, {
+        fill: 'yellow',
+        strokeWidth: 1,
+        selectable: false
+      });
 	},
-
-	saveCorner: function() {
-		configuration.set(key, value);
-	},
-	
   });
 
-  var CornersView = ScreenView.extend({
+  var RectifyView = ScreenView.extend({
     template: template('#corners-template'),
     render: function() {
       this.$el.html(this.template());
@@ -108,9 +143,9 @@ $(function() {
 
   var Router = Backbone.Router.extend({
     views: {
-      start: StartView,
-      overview: OverviewView,
-      corners: CornersView,
+      start: new StartView(),
+      overview: new OverviewView(),
+      corners: new RectifyView(),
     },
 
     initialize: function(options) {
@@ -119,8 +154,8 @@ $(function() {
     },
 
     renderView: function(name) {
-      viewClass = name === '' ? StartView : this.views[name];
-      return (new viewClass()).render();
+      name = name || 'start';
+      return this.views[name].render();
     },
   });
 
@@ -129,4 +164,5 @@ $(function() {
   });
   router = new Router({ navigation: navigation });
   Backbone.history.start();
+  Backbone.history.navigate('');
 });
